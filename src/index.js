bfc7daae98f110d23d5fb6997449139b766e123a
JinJoa <36723194+JinJoa@users.noreply.github.com>
2025-01-15 18:25:16 +0900

    ReactDOM.render 대체

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("potato"));
+// ReactDOM.render(<App />, document.getElementById("potato"));
+const root = ReactDOM.createRoot(document.getElementById('root')); // root 생성
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
